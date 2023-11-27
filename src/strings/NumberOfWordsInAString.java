9a43726cb173adfd511b294f14eaed3dbf180630
Gouse <professionalgouse123@gmail.com>
2023-11-28 00:04:13 +0530

    First commit

---
+++ src/strings/NumberOfWordsInAString.java
@@ -0,0 +1,21 @@
+package strings;
+
+public class NumberOfWordsInAString 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		String s = "This is a string";
+		
+		String[] arrOfStr = s.split("\\s");
+		
+		 int totalWord = arrOfStr.length;
+		 
+		 System.out.println("Total words :"+  totalWord);
+		
+
+	}
+
+}
